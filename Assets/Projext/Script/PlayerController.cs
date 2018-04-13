8e1f0f376dd9c7fec74511ff798c2f4378651d9e
LIVEU-TAD\andres.perez <andresperezmartinezarias@gmail.com>
2018-04-13 10:59:41 +0200

    Tiles maps, Link spritesheet, intento de animaciones

---
+++ Assets/Projext/Script/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 4f;
+    
+
+	void Start () {
+		
+
+	}
+	
+	void Update () {
+
+		
+	}
+}
